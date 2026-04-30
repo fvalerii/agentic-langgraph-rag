42756445ba970b0331cba8d7c1d65cb1782c27bd
Fabrizio Valerii <fvalerii@gmail.com>
2026-04-30 20:36:49 +0200

    Add files via upload

--- document_processor/file_handler.py
+++ document_processor/file_handler.py
@@ -7,7 +7,7 @@ from typing import List
 from docling.document_converter import DocumentConverter
 from langchain_text_splitters import MarkdownHeaderTextSplitter
 from config.settings import settings
-from utils.logging import logger
+from utils.logging_config import logger
 
 class DocumentProcessor:
     def __init__(self):
